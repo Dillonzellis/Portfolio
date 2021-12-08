bcbde9b16327fad4af57abe850751c453b77c3d3
Dillon Ellis <dillonzellis@gmail.com>
2021-12-07 21:54:58 -0500

    Add gulp functions

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,53 @@ const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
 const terser = require("gulp-terser");
 const browsersync = require("browser-sync");
+const del = require("del");
 
 // Sass Task
 function scssTask() {
-  return src("app/scss/style.scss", { sourcemaps: true })
+  return src("app/scss/main.scss", { sourcemaps: true })
     .pipe(sass())
     .pipe(postcss([cssnano]))
     .pipe(dest("dist", { sourcemaps: "." }));
 }
 
+// JS Task
+function jsTask() {
+  return src("app/js/main.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest("dist", { sourcemaps: true }));
+}
+
+// Browsersync Tasks
+function browsersyncServer(cb) {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  cb();
+}
+
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch("*.html", browsersyncReload);
+  watch(
+    ["app/scss/**/*.scss", "app/js/**/*.js"],
+    series(scssTask, jsTask, browsersyncReload)
+  );
+}
+
+// Deleting dist folder
+function cleanDist() {
+  return del("./dist");
+}
+
 // Default Gulp Task
-exports.default = scssTask;
+exports.default = series(scssTask, jsTask, browsersyncServer, watchTask);
+
+exports.cleanDist = cleanDist;
