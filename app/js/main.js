f7b2f6cc2bd028427159deb2b9a0cbc79f8b42bb
Dillon Ellis <dillonzellis@gmail.com>
2021-12-08 20:27:21 -0500

    Navbar link highlight

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,10 @@
 console.log("yeppers");
 
+// Sticky Nav
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -12,8 +15,31 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
+//nav bar link current class
+const sections = document.querySelectorAll("section");
+const navlinks = document.querySelectorAll("a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navlinks.forEach((li) => {
+    li.classList.remove("active");
+    const href = li.getAttribute("href").substring(1);
+    if (href === current) {
+      li.classList.add("active");
+    }
+  });
+});
