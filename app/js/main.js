2f5b5c174237052a021132b55f731ef960c8d014
Dillon Ellis <dillonzellis@gmail.com>
2021-12-09 02:18:48 -0500

    Skills page styling

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,28 @@
 // Sticky Nav
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-  myFunction();
-};
+// window.onscroll = function () {
+//   myFunction();
+// };
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+// var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 
 //nav bar link current class
 const sections = document.querySelectorAll("section");
 const navlinks = document.querySelectorAll("a");
+const spacer = document.querySelector(".skills-spacer");
 
 window.addEventListener("scroll", () => {
   let current = "";
@@ -39,5 +40,29 @@ window.addEventListener("scroll", () => {
     if (href === current) {
       li.classList.add("active");
     }
+    // if (href === "skills") {
+    //   spacer.classList.add("spacer-height");
+    // }
   });
 });
+
+//remove navbar from home screen
+// const nav = document.querySelector("#navbar");
+
+// window.addEventListener("scroll", () => {
+//   console.log(scrollY);
+
+//   if (nav.classList.contains("hidden")) {
+//     if (scrollY >= 749) {
+//       nav.classList.remove("hidden");
+//     }
+//   }
+
+//   if (scrollY <= 749) {
+//     nav.classList.add("hidden");
+//   }
+
+// if (scrollY < 750) {
+//   nav.classList.add("hidden");
+// } else
+// });
